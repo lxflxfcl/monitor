c2c6dddb51a6109e90a7076de77578fd8b9c966f
lxflxfcl <1628672871@qq.com>
2023-01-15 10:34:22 +0800

    修改installDb.py文件位置

--- installDb.py
+++ installDb.py
@@ -6,11 +6,11 @@ import os
 '''
 
 step = 1
-if os.path.isfile('../../cve_db.db'):
+if os.path.isfile('cve_db.db'):
     step = 2
 
 if step == 1:
-    con = sqlite3.connect("../../cve_db.db")
+    con = sqlite3.connect("cve_db.db")
     cursor = con.cursor()
     cursor.execute('create table cve_info(vuln_name TEXT, url TEXT, cnnvd_num TEXT, danger_level TEXT, cve_num TEXT, vuln_type TEXT, release_time TEXT, thread_type TEXT, updated_time TEXT, company TEXT, vuln_desc TEXT, vuln_announce TEXT, reference_url TEXT, influnce_product TEXT, patch TEXT)')
     cursor.execute('create table cve_info_ms(release_data TEXT,cve_num TEXT,cve_title TEXT,desc TEXT,mitre_url TEXT,tag TEXT)')
@@ -20,10 +20,10 @@ if step == 1:
     step = 2
 
 if step == 2:
-    if os.path.isdir('../../log'):
+    if os.path.isdir('log'):
         print("log目录已存在！！")
         print("安装完成！！")
     else:
-        os.mkdir('../../log') #创建用于存放日志的目录
+        os.mkdir('log') #创建用于存放日志的目录
         print('创建了log目录！！')
         print("安装完成！！")
